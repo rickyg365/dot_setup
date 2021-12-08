ca505bb7a2c6e5e76e6e6f72f1c04559d0d6f8c6
rickyg365 <rickygonzal3z@hotmail.com>
2021-12-07 20:53:15 -0800

    first tmux script

---
+++ tmucs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create new session
+session="coding"
+
+# Start session, detached
+tmux new-session -d -s $session
+
+# Rename Initial Window(0)
+tmux rename-window -t 0 'Main'
+
+# Start up work directory
+tmux send-keys -t 'Main' 'clear' C-m 'cd ~' C-m
+
+tmux splitw -h
+
+# Select pane 2
+tmux selectp -t 1
+tmux send-keys 'clear' C-m 'cd ~ && ls' C-m
+
+# Split pane 2 vertically by 25%
+tmux splitw -v -p 25
+tmux selectp -t 2
+tmux send-keys 'clear' C-m 'cd ~ && ls' C-m
+
