7c9ab80a8f81fa45ff371f70f7f72a7df1d15f45
rickyg365 <rickygonzal3z@hotmail.com>
2021-12-08 09:23:40 -0800

    bug fix mnav, tmucs

--- tmucs.sh
+++ tmucs.sh
@@ -23,3 +23,9 @@ tmux splitw -v -p 25
 tmux selectp -t 2
 tmux send-keys 'cd ~' C-m 'clear && ls' C-m
 
+# select first pane
+tmux selectp -t 0
+
+# # New window
+# tmux new-window -t ${session}:
+# tmux rename-window -t 1 'editor'
