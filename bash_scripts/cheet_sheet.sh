02c965188d7199a2837d7219bedb7d63f2467ea4
rickyg365 <rickygonzal3z@hotmail.com>
2021-12-10 16:23:17 -0800

    changes

--- bash_scripts/cheet_sheet.sh
+++ bash_scripts/cheet_sheet.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
-# Version='0.1'
-
+# Version='1.0'
 # curl cht.sh/{language}/query+string
 # curl cht.sh/{core-util}~operation
 
 # curl cht.sh/{language}/learn:query+string
 
 
-# Options
 languages=`echo "rust python javascript nodejs typescript c cpp" | tr ' ' '\n'`
 
-selected=`printf "$languages" | fzf`
-echo "selected $selected"
 
+selected=`printf "$languages" | fzf`
 read -p "query: " query
-echo "$query"
 
 echo "curl cht.sh/$selected/`echo $query | tr ' ' '+'`"
-command="curl cht.sh/$selected/`echo $query | tr ' ' '+'`"
-# tmux send-keys -t 'Main' 'clear && ls' C-m "$command" C-m 
+
+
 curl cht.sh/$selected/`echo "$query" | tr ' ' '+'`
-
-
-# if printf $languages | grep -qs $selected; then
-
-#     # Start up work directory
-    
-# else
-#     # Core util
-#     echo "core"
-#     curl cht.sh/$selected~$query
-# fi
-
 
