542622ef9550f6cb377c4e8680894533c0915494
rickyg365 <rickygonzal3z@hotmail.com>
2022-03-03 23:49:06 -0800

    up

--- finalize/bash/get_anime.sh
+++ finalize/bash/get_anime.sh
@@ -4,14 +4,18 @@
 
 TERMUX_ABS="/data/data/com.termux/files/home/storage/downloads/anime"
 
-DEFAULT="$HOME/downloads/anime"
+DEFAULT_ANIME="$HOME/downloads/anime"
 
 # GoTo anime dir path
-check_dir($DEFAULT) && cd $DEFAULT
+[ ! -d $DEFAULT_ANIME ] && mkdir -p $DEFAULT_ANIME
+cd $DEFAULT_ANIME
+# check_dir($DEFAULT_ANIME) && cd $DEFAULT_ANIME
 
 # GoTo/Create title dir
 TITLE=$1
-check_dir($TITLE) && cd $TITLE
+[ ! -d $TITLE ] && mkdir -p $TITLE
+cd $TITLE
+# check_dir($TITLE) && cd $TITLE
 
 ani-cli -d $TITLE
 
