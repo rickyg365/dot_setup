48b59c7f9bf408b8039f29fef09fb2bbe0eda575
rickyg365 <rickygonzal3z@hotmail.com>
2022-03-03 23:43:10 -0800

    updoot

--- finalize/bash/get_anime.sh
+++ finalize/bash/get_anime.sh
@@ -4,13 +4,12 @@ check_dir() {
     [ ! -d $1 ] && mkdir -p $1
 }
 
-DEFAULT="~/anime"
 TERMUX_ABS="/data/data/com.termux/files/home/storage/downloads/anime"
 
-CDIR="$HOME/downloads/anime"
+DEFAULT="$HOME/downloads/anime"
 
 # GoTo anime dir path
-check_dir($CDIR) && cd $CDIR
+check_dir($DEFAULT) && cd $DEFAULT
 
 # GoTo/Create title dir
 TITLE=$1
